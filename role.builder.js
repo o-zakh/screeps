7c4a31efe9afb99f6be55a5a754b2e9c82194015
o-zakh <mrunenlightenment@gmail.com>
2026-06-02 17:55:41 +0400

    RCL-2/3

--- role.builder.js
+++ role.builder.js
@@ -3,6 +3,8 @@ var roleBuilder = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
+		const builderEnergySourceId = "5bbcac359099fc012e6351a1"
+
 	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.building = false;
             creep.say('🔄 harvest');
@@ -18,20 +20,25 @@ var roleBuilder = {
 				if (targets.some(target => target.structureType == "extension")) {
 					targets = _.filter(targets, (construction) => construction.structureType == STRUCTURE_EXTENSION)
 				}
-			}
-			
-			targets = creep.pos.findClosestByPath(targets, {algorithm: 'dijkstra'});
+				targets = creep.pos.findClosestByPath(targets, {algorithm: 'dijkstra'});
 
-            if(targets) {
-                if(creep.build(targets) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets, {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
+				if(targets) {
+					if(creep.build(targets) == ERR_NOT_IN_RANGE) {
+						creep.moveTo(targets, {visualizePathStyle: {stroke: '#ffffff'}});
+					}
+				}
+			} else {
+				var repairTargets = _.filter(creep.room.find(FIND_STRUCTURES), (structure) => structure.hitsMax - structure.hits > 500)
+				repairTarget = creep.pos.findClosestByPath(repairTargets, {algorithm: 'dijkstra'});
+				if(creep.repair(repairTarget) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(repairTarget, {visualizePathStyle: {stroke: '#ffffff'}});
+				}
+			}
 	    }
 	    else {
-	        var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+	       var source = Game.getObjectById(builderEnergySourceId)
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
 	    }
 	}
